99965e063a4badd4ec7003ddb31f816c3a2c26f6
famanakis <73799603+famanakis@users.noreply.github.com>
2023-01-31 22:08:57 -0700

    upload

---
+++ tailwind_ui_components/contact_forms/collection.js
@@ -0,0 +1,9 @@
+import VerticalContactForm from "./vertical_contact_form";
+import contactForm from "./standard_contact_form";
+
+const contactFormsCollection = [
+  {componentName: "Vertical contact form", component: VerticalContactForm},
+  {componentName: "Sample Contact form", component: contactForm},
+];
+
+export default contactFormsCollection;
